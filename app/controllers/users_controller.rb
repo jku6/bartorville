6406e5ab5d4fea5c8d17ba1e9f934f819c3515a5
Avinash Karnani <avi@avikarnani.com>
2012-11-26 17:05:41 -0500

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 		@user = User.new(params[:user])
 
 		if @user.save
-			redirect_to users_path #MATCH THIS UP
+			redirect_to users_path(@user) 
 		else
 			render :new
 		end
@@ -26,68 +26,30 @@ class UsersController < ApplicationController
 	def destroy
     	user = User.find(params[:id])
     	user.delete
-    	redirect_to user
+       	redirect_to user  
 	end
 
 #Standard stuff above
-
-
-
-	def login
-		user = User.find_by_username(params[:username])
-		if user && username(params[:username])
-
-
-
-
-	def edit
-		user_id = params[:id]
-		@user = User.find(user_id)
-	end
-	def update
-		@user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      redirect_to @user
-    else
-      render :edit
-    end
-	end
-end
-
-
-
-  def login
-        user = User.find_by_username(params[:username])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            redirect_to dashboard_index_path(user)
-        else
-            render :new #this is the new.html.erb page
-        end
-    end
-
-  def show
-      user_id = params[:id]
-      @user = User.find(user_id)
-  end
-
-
 
   def update
       @user = User.find(params[:id])
       if @user.update_attributes(params[:user])
         session[:user_id] = @user.id
+        # check paths and match up  =========================
         redirect_to dashboard_index_path(@user) 
-      else
+             else
         render :edit
       end
   end
 
 
-  
-
-  # private
-  # def ensure_logged_in
-  #      redirect_to root_path if @auth.nil?
-  #  end
+  def login
+        user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to dashboard_index_path(user)
+        else
+            render :new #this is the new.html.erb page =======
+        end
+    end
 end
