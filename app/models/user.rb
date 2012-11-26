a205c93bd959db948093a1deac187fe8ace650bf
Avinash Karnani <avi@avikarnani.com>
2012-11-26 16:20:16 -0500

    pulling for annotation error fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)
+#  password_digest :string(255)
+#  email           :string(255)
+#  user_photo      :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
 	has_many :items
 	has_secure_password
