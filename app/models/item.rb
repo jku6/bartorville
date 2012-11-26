a29511ca1ffe1e55ad8f693483345cc53bca463c
Jonathan Ku <jonathanku88@gmail.com>
2012-11-26 16:19:35 -0500

    image uploader

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
-class Item < ActiveaRecord::Base
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Item < ActiveRecord::Base
   belongs_to :user
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id         :integer          not null, primary key
+#  caption    :string(255)
+#  image      :string(255)
+#  item_id    :integer
+#  is_primary :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Photo < ActiveRecord::Base
     belongs_to :item
     mount_uploader :image, ImageUploader
