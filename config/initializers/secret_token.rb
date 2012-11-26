6f4a4fc36ceac85bd6c4b789069a3d41db777972
Jonathan Ku <jonathanku88@gmail.com>
2012-11-26 15:18:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bartorville::Application.config.secret_token = '758701fb7edf53f09bcb942f82d6a2b79b4b06105227b9c1096bd337ffd3f4e47389a2786aa1f86515b4132edd53b3f38b0201cf4248deb77cbb21960c2d0e27'
